8757d5cffcbc8b4a2c447c50e52d950755231337
SherlockStd <services@sherlockstd.io>
2017-01-09 14:32:50 +0100

    Move test.js to helpers/tests/blockchain_test.js

--- helpers/tests/blockchain_test.js
+++ helpers/tests/blockchain_test.js
@@ -1,37 +1,39 @@
-var blockchain = require('./helpers/blockchain');
+var blockchain = require('../blockchain');
 var from = "9120485202270553493L";
 var to = "7023069056644097238L";
 
 blockchain.getTxsFromTo(from, to, function (err, data) {
+    console.log("\ngetTxsFromTo() : \n");
     if (err) {
-        return (err);
+        console.log(err);
     } else {
-        console.log("\ngetTxsFromTo() :");
         console.log(data);
     }
 });
 
 blockchain.getBalance(from, function (err, data) {
+    console.log("\ngetBalance() : \n");
     if (err) {
-        return (err);
+        console.log(err);
     } else {
-        console.log("\ngetBalance() : \n" + data);
+        console.log(data);
     }
 });
 
 blockchain.getAccount(from, function (err, data) {
+    console.log("\ngetAccount() : \n");
     if (err) {
-        return (err);
+        console.log(err);
     } else {
-        console.log("\ngetAccount() :");
         console.log(data);
     }
 });
 
 blockchain.getAmountFromTo(from, to, function (err, data) {
+    console.log("\ngetAmountFromTo() : \n");
     if (err) {
-        return (err);
+        console.log(err);
     } else {
-        console.log("\ngetAmountFromTo() : \n" + data);
+        console.log(data);
     }
 });
