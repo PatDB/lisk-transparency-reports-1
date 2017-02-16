bb8025001bd6985028b8b8c5ef94981c2e4f4fa5
SherlockStd <sherlock@sherlockstd.io>
2017-02-16 17:17:18 +0100

    Rebrand error handler function Related to #14

--- app.js
+++ app.js
@@ -44,7 +44,6 @@ app.use(function (req, res, next) {
 
 // error handler
 app.use(function (err, req, res, next) {
-  // render the error page
   res.status(err.status || 500).json(err.message || 'En errror occured.')
 })
 
