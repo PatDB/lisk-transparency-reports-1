adaace944b5adbd97be4c8b47e1bbfab27075497
patrizia <patrizia@made4web.it>
2017-03-01 11:17:22 +0100

    #39 changed structure after new definition of ref

--- models/Transaction.js
+++ models/Transaction.js
@@ -1,42 +1,56 @@
 const mongoose = require('mongoose')
-const mongoosePaginate = require('mongoose-paginate')
 const Schema = mongoose.Schema
-const Usermodel = require('./User.js')
+
 //const mongoosePaginate = require('mongoose-paginate')
 
 //typetx schema
 const typeTxSchema = new Schema({
+  
   typetx: {
     type: String,
     required: true
   },
-  confirmed: {
+  
+  typeconfirmed: {
     type: Boolean,
     required: true,
     default: true
   },
-  ordertx: {
+  
+  typeorder: {
     type: Number
   }
+
 })
+
+
 
 // Transaction schema
 const TransactionSchema = new Schema({
-  address: {
-    type: String,
-     lowercase: true,
-    required: true
-  },
-  
   idTx: {
     type: Number,
     unique: true,
     required: true
   },
   
+  txSender: {
+    _delegate: [{ type: Schema.Types.ObjectId, ref: 'User' ,require = true}], // Join
+    _address: [{ type: Schema.Types.ObjectId, ref: 'Address' , require = true }] // Join
+  },
+  
+  txRecipient: {
+    delegate: String,//If existing in DB, else check blockchain
+    address: String // Just text, no join
+  },
+  
+  txAmount: {
+    type: Number,
+    required: true
+  },
+  
   typeTx: [typeTxSchema] ,//set default?
   
-  insDate: {
+  txInsDate: {
     type: Date,
     default: Date.Now,//date of definition or date of insert?
     required: true
@@ -44,6 +58,9 @@ const TransactionSchema = new Schema({
 }, 
 {
   timestamps: true
-})
+}
+       
+)
+
 
 module.exports = mongoose.model('Transaction', TransactionSchema)
